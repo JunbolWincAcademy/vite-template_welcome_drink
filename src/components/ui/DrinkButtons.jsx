0e03bbfc3cbf70e58dbf748e565d74864630a780
Junier Bolivar <junierba@yahoo.co.uk>
2024-01-29 23:02:08 +0100

    add props deconstruction

--- src/components/ui/DrinkButtons.jsx
+++ src/components/ui/DrinkButtons.jsx
@@ -1,8 +1,9 @@
-export const DrinkButtons = (props) => {
+// export const DrinkButtons = (props) => {// here props is a parameter
+  export const DrinkButtons = ({drinkOne, drinkTwo}) => {// here props is a parameter
   // before it was no parameter
   const Choose = 'Hello!, would you like coffee or thee?';
-  const tea = 'Tea';
-  const coffee = 'Coffee';
+  // const tea = 'Tea';
+  // const coffee = 'Coffee';
 
   return (
     <>
@@ -10,8 +11,10 @@ export const DrinkButtons = (props) => {
         <h2>{Choose}</h2>
       </div>
       <div className="button-group">
-        <button className="btn_thee">{props.drinkOne}</button>
-        <button className="btn_coffee">{props.drinkTwo}</button>
+      <button className="btn_thee">{drinkOne}</button>
+        <button className="btn_coffee">{drinkTwo}</button>
+        {/* <button className="btn_thee">{props.drinkOne}</button>
+        <button className="btn_coffee">{props.drinkTwo}</button> */}
 
         {/* <button className="btn_thee"><p className= "btn_color">{tea}</p></button>
         <button className="btn_coffee"><p className= "btn_color">{coffee}</p></button> */}
