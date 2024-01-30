ed1d30d1c2549e680f19cc0eeb6f0527fb0043ea
Junier Bolivar <junierba@yahoo.co.uk>
2024-01-30 17:41:38 +0100

    fix some stuff

--- src/components/ui/DrinkButtons.jsx
+++ src/components/ui/DrinkButtons.jsx
@@ -1,5 +1,8 @@
 // export const DrinkButtons = (props) => {// here props is a parameter
-  export const DrinkButtons = ({drinkOne, drinkTwo}) => {// here props is a parameter
+
+
+export const DrinkButtons = ({ drinkOne, drinkTwo }) => {
+  // here props is a parameter
   // before it was no parameter
   const Choose = 'Hello!, would you like coffee or thee?';
   // const tea = 'Tea';
@@ -11,14 +14,17 @@
         <h2>{Choose}</h2>
       </div>
       <div className="button-group">
-      <button className="btn_thee">{drinkOne}</button>
+        <button className="btn_thee">{drinkOne}</button>
         <button className="btn_coffee">{drinkTwo}</button>
-        {/* <button className="btn_thee">{props.drinkOne}</button>
-        <button className="btn_coffee">{props.drinkTwo}</button> */}
 
-        {/* <button className="btn_thee"><p className= "btn_color">{tea}</p></button>
-        <button className="btn_coffee"><p className= "btn_color">{coffee}</p></button> */}
       </div>
     </>
+    //     {/* <button className="btn_thee">{props.drinkOne}</button>
+    //     <button className="btn_coffee">{props.drinkTwo}</button> */}
+
+    //     {/* <button className="btn_thee"><p className= "btn_color">{tea}</p></button>
+    //     <button className="btn_coffee"><p className= "btn_color">{coffee}</p></button> */}
+    //   </div>
+    // </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App'; // they asked not to import it like this
+
 import { App } from './App'; //but like this but I don't see anymore the text Welcome!
 
 import './index.css';
